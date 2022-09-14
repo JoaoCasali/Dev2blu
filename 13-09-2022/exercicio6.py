4ba96bb54b9816ef493b3f9a3fafddbe2d6620a2
JoaoCasali <joaovitorcasali@gmail.com>
2022-09-13 21:59:19 -0300

    pequenas mudanças

--- 13-09-2022/exercicio6.py
+++ 13-09-2022/exercicio6.py
@@ -2,7 +2,8 @@ poli = "="*8
 print(f'{poli} CABEÇALHO {poli}')
 
 for i in range(0, 1501, 2):
-    print(i)
+    if i % 2 == 0:
+        print(i)
 
 print("PARABÉNS, VOCÊ CONSEGUIU!!")
 print(f'{poli} RODAPÉ {poli}')
---
+++ 13-09-2022/exercicio7.py
@@ -0,0 +1,6 @@
+poli = "="*8
+num = int(input("Digite um número: "))
+print(f'{poli} TABUADA {poli}')
+for i in range(0,11):
+    print(f"{num} X {i} = {num*i}")
+print(f'{poli} TABUADA {poli}')
